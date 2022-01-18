4ee61593c2794ccac9cd14287d10c82dde00e418
Tomosuke0930 <84496536+Tomosuke0930@users.noreply.github.com>
2022-01-18 09:28:46 +0900

    create home layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,120 +1,81 @@
-import { Text, HStack, Button,Link, Flex, Heading, Box, ButtonGroup, Stack, useColorModeValue, IconButton, useBreakpointValue } from '@chakra-ui/react';
+import {
+  Text,
+  HStack,
+  Button,
+  Link,
+  Flex,
+  Heading,
+  Box,
+  ButtonGroup,
+  Stack,
+  useColorModeValue,
+  IconButton,
+  useBreakpointValue,
+  Grid,
+  GridItem,
+  Spacer,
+  chakra,
+} from "@chakra-ui/react";
 
+import NextLink from "next/link";
 
-import NextLink from 'next/link';
-
-
- function Header() {
-     return <header>
-         <Flex
-      as="nav"
-      align="center"
-      justify="space-between"
-      wrap="wrap"
-      padding={4}
-      bg="blackAlpha.900"
-      color="white"
+function Header() {
+  return (
+    <chakra.header 
+    // position="fixed" w="full"
     >
-    
-
-     
-      <HStack fontSize={12} color="#FFFAF0">
-      <Heading as="h4" mr={20} size="md" >
-          Tomo Finance
-        </Heading>
-
-            <NextLink href='/' passHref >
-                <Link p={2}>Home</Link>
-            </NextLink>
-            <NextLink href='/cemestry' passHref>
-                <Link p={2}>CEMESTRY</Link>
-            </NextLink>
-            <NextLink href='/masonry' passHref>
-                <Link p={2}>MASONRY</Link>
-            </NextLink>
-            <NextLink href='/pit' passHref>
-                <Link p={2}>PIT</Link>
-            </NextLink>
-            <NextLink href='/sbs' passHref>
-                <Link p={2}>SBS</Link>
-            </NextLink>
-            <NextLink href='/liquidity' passHref>
-                <Link p={2}>LIQUIDITY</Link>
-            </NextLink>
-            <NextLink href='/regurations' passHref>
-                <Link p={2}>REGULATIONS</Link>
-            </NextLink>
-            <NextLink href='/bthlabo' passHref>
-                <Link>BTHLabo</Link>
-            </NextLink>
-            <ButtonGroup 
-                ml={4} 
-                color="blackAlpha.700" 
-                backgroundColor="whiteAlpha.900"
-                rounded="md"
-                size="xs"
-            >
-                <Button>MY WALLET</Button>
-            </ButtonGroup>
-        </HStack>
-    </Flex>  
-        {/* <Box>
       <Flex
-        bg={useColorModeValue('white', 'gray.800')}
-        color={useColorModeValue('gray.600', 'white')}
-        minH={'60px'}
-        py={{ base: 2 }}
-        px={{ base: 4 }}
-        borderBottom={1}
-        borderStyle={'solid'}
-        borderColor={useColorModeValue('gray.200', 'gray.900')}
-        align={'center'}>
-        <Flex
-          flex={{ base: 1, md: 'auto' }}
-          ml={{ base: -2 }}
-          display={{ base: 'flex', md: 'none' }}>
-        </Flex>
-        <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
-          <Text
-            textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
-            fontFamily={'heading'}
-            color={useColorModeValue('gray.800', 'white')}>
-            Logo
-          </Text>
+        as="nav"
+        padding={4}
+        // bg="blackAlpha.900"
+        bg="red"
+        color="white"
+      >
+        <Box colSpan={2}>
+          <Heading as="h2" size="md">
+            Tomo Finance
+          </Heading>
+        </Box>
+          <Spacer></Spacer>
+ 
 
-          <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
-          </Flex>
-        </Flex>
-
-        <Stack
-          flex={{ base: 1, md: 0 }}
-          justify={'flex-end'}
-          direction={'row'}
-          spacing={6}>
-          <Button
-            as={'a'}
-            fontSize={'sm'}
-            fontWeight={400}
-            variant={'link'}
-            href={'#'}>
-            Sign In
-          </Button>
-          <Button
-            display={{ base: 'none', md: 'inline-flex' }}
-            fontSize={'sm'}
-            fontWeight={600}
-            color={'white'}
-            bg={'pink.400'}
-            href={'#'}
-            _hover={{
-              bg: 'pink.300',
-            }}>
-            Sign Up
-          </Button>
-        </Stack>
+        <Box colSpan={6} fontSize={12} >
+          <NextLink href="/" passHref>
+            <Link p={2}>Home</Link>
+          </NextLink>
+          <NextLink href="/cemestry" passHref>
+            <Link p={2}>CEMESTRY</Link>
+          </NextLink>
+          <NextLink href="/masonry" passHref>
+            <Link p={2}>MASONRY</Link>
+          </NextLink>
+          <NextLink href="/pit" passHref>
+            <Link p={2}>PIT</Link>
+          </NextLink>
+          <NextLink href="/sbs" passHref>
+            <Link p={2}>SBS</Link>
+          </NextLink>
+          <NextLink href="/liquidity" passHref>
+            <Link p={2}>LIQUIDITY</Link>
+          </NextLink>
+          <NextLink href="/regurations" passHref>
+            <Link p={2}>REGULATIONS</Link>
+          </NextLink>
+          <NextLink href="/bthlabo" passHref>
+            <Link>BTHLabo</Link>
+          </NextLink>
+          <ButtonGroup
+            ml={3}
+            color="blackAlpha.700"
+            backgroundColor="whiteAlpha.900"
+            rounded="md"
+            size="xs"
+          ><Button>MY WALLET</Button>
+          </ButtonGroup>
+        </Box>
       </Flex>
-    </Box> */}
-    </header>;
-   }
+    </chakra.header>
+  );
+}
 
-   export default Header;
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,147 +1,263 @@
-import { WarningIcon, WarningTwoIcon } from '@chakra-ui/icons'
-import { Box, Button, ButtonGroup, Center, Divider, Flex, HStack, Image,SimpleGrid,Spacer,Stack,Text } from '@chakra-ui/react'
-import Link from 'next/link'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
-
+import { WarningIcon, WarningTwoIcon } from "@chakra-ui/icons";
+import {
+  AbsoluteCenter,
+  Box,
+  Button,
+  ButtonGroup,
+  Center,
+  Divider,
+  Flex,
+  Grid,
+  GridItem,
+  HStack,
+  Image,
+  SimpleGrid,
+  Spacer,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 const Home = () => (
-  <Box 
-  bgImage="https://external-preview.redd.it/Y3bRw6FPYPdvBiw71-1E6Uh5fSvKgq8PB4nr8qJKeAc.jpg?auto=webp&s=e29f813bf0a3ee6fc8561a7a93071021323a3a00"
-  bgRepeat="no-repeat"
-  bgSize="cover"
+  <Box
+    
+    bgImage="https://external-preview.redd.it/Y3bRw6FPYPdvBiw71-1E6Uh5fSvKgq8PB4nr8qJKeAc.jpg?auto=webp&s=e29f813bf0a3ee6fc8561a7a93071021323a3a00"
+    bgRepeat="no-repeat"
+    bgSize="cover"
   >
     <Header />
-   
-    <HStack m={10}>
-    <Image 
-    src='https://tomb.finance/static/media/crypto_tomb_cash.f2b44ef4.svg' 
-    alt='tomb' 
-    boxSize='270px'
-    />
-    <Box backgroundColor={'white'} opacity="90%" p={4} rounded="md">
-    
-    <Text color={'blackAlpha.700'} fontSize="lg" pb={3}>Welcome to Tomb Finance</Text>
-    <Text color={'blackAlpha.700'} fontSize="xs" pb={2}>
-    The first algorithmic stablecoin on Fantom Opera, pegged to the price of 1 FTM via seigniorage.
 
-    </Text>
+    <HStack mx={16}>
+      <Image
+        src="https://tomb.finance/static/media/crypto_tomb_cash.f2b44ef4.svg"
+        alt="tomb"
+        boxSize="270px"
+      />
+      <Box backgroundColor={"white"} opacity="90%" p={4} rounded="md">
+        <Text color={"blackAlpha.700"} fontSize="lg" pb={3}>
+          Welcome to Tomb Finance
+        </Text>
+        <Text color={"blackAlpha.700"} fontSize="xs" pb={2}>
+          The first algorithmic stablecoin on Fantom Opera, pegged to the price
+          of 1 FTM via seigniorage.
+        </Text>
 
-    <Text color={'blackAlpha.700'} fontSize="xs">
-    Stake your TOMB-FTM LP in the Cemetery to earn TSHARE rewards. Then stake your earned TSHARE in the Masonry to earn more TOMB
-    </Text>
-    </Box>
+        <Text color={"blackAlpha.700"} fontSize="xs">
+          Stake your TOMB-FTM LP in the Cemetery to earn TSHARE rewards. Then
+          stake your earned TSHARE in the Masonry to earn more TOMB
+        </Text>
+      </Box>
     </HStack>
     <Center>
-    <Box bg={'orange.400'} rounded="sm">
-      <HStack>
-      <WarningTwoIcon color="black" pl={2} w={5} h={5}/>
-      <Text color={'blackAlpha.700'} fontWeight="bold" fontSize={13} pr={2} letterSpacing="tight" py={2} >Please visit our documentation before purchasing TOMB or TSHARE</Text>
-      </HStack>
-    </Box>
+      <Box bg={"orange.400"} rounded="sm">
+        <HStack>
+          <WarningTwoIcon color="black" pl={2} w={5} h={5} />
+          <Text
+            color={"blackAlpha.700"}
+            fontWeight="bold"
+            fontSize={13}
+            pr={2}
+            letterSpacing="tight"
+            py={2}
+          >
+            Please visit our documentation before purchasing TOMB or TSHARE
+          </Text>
+        </HStack>
+      </Box>
     </Center>
 
-    <SimpleGrid columns={2} my={6} mx={2} spacing={10}>
-      <Box backgroundColor={'white'}  opacity="90%" rounded="md" p={4} width="33%">
-        <Text color={'blackAlpha.700'} fontSize="sm">Total Value Locked</Text>
-        <Text color={'blackAlpha.700'} fontSize="sm">$1,434,811,655</Text>
-      </Box>
-      <Box display="flex" backgroundColor={'white'} opacity="90%" p={4} rounded="md"> 
+    <Grid templateColumns="repeat(3, 1fr)" mt="8" gap={4}>
+      <GridItem
+        colSpan={1}
+        backgroundColor={"white"}
+        opacity="90%"
+        rounded="md"
+        p={4}
+        textAlign="center"
+        ml="16"
+      >
+        <Text color={"blackAlpha.700"} fontSize="sm">
+          Total Value Locked
+        </Text>
+        <Text color={"blackAlpha.700"} fontSize="sm">
+          $1,434,811,655
+        </Text>
+      </GridItem>
+      <GridItem
+        colSpan={2}
+        display="flex"
+        backgroundColor={"white"}
+        opacity="90%"
+        p={4}
+        rounded="md"
+        justifyContent="center"
+        mr="16"
+      >
         <HStack>
-      <Button letterSpacing="tight" size='xs' bg="#561EB0" color="white" rounded="sm" _hover={{ bg: '#000' }}>STAKE NOW</Button>
-      <Button letterSpacing="tight" size='xs'  bg="#CBD5E0" rounded="sm" color="blackAlpha.900" _hover={{ color: 'white', bg:'gray.400' }}>FARM NOW</Button>
-      <Button letterSpacing="tight" size='xs'  bg="#CBD5E0" rounded="sm" color="blackAlpha.900" _hover={{ color: 'white', bg:'gray.400' }}>BUY TOMO</Button>
-      <Button letterSpacing="tight" size='xs'bg="#561EB0" color="white" rounded="sm" _hover={{ bg: '#000' }}>BUY TOMOSHARE</Button>
-      </HStack>
-      </Box>
-      
-    </SimpleGrid>  
+          <Button
+            letterSpacing="tight"
+            size="xs"
+            bg="#561EB0"
+            color="white"
+            rounded="sm"
+            _hover={{ bg: "#000" }}
+          >
+            STAKE NOW
+          </Button>
+          <Button
+            letterSpacing="tight"
+            size="xs"
+            bg="#CBD5E0"
+            rounded="sm"
+            color="blackAlpha.900"
+            _hover={{ color: "white", bg: "gray.400" }}
+          >
+            FARM NOW
+          </Button>
+          <Button
+            letterSpacing="tight"
+            size="xs"
+            bg="#CBD5E0"
+            rounded="sm"
+            color="blackAlpha.900"
+            _hover={{ color: "white", bg: "gray.400" }}
+          >
+            BUY TOMO
+          </Button>
+          <Button
+            letterSpacing="tight"
+            size="xs"
+            bg="#561EB0"
+            color="white"
+            rounded="sm"
+            _hover={{ bg: "#000" }}
+          >
+            BUY TOMOSHARE
+          </Button>
+        </HStack>
+      </GridItem>
+    </Grid>
 
-    
-    <SimpleGrid columns={3} m={4} spacing={30}>
-
-        <Box textAlign="center"  backgroundColor={'white'} color="black" opacity="90%" rounded="md" p={3} >
-        <Stack>
+    <SimpleGrid columns={3} gap={4} my={4} mx={16}>
+      <Box
+        textAlign="center"
+        backgroundColor={"white"}
+        color="black"
+        opacity="90%"
+        rounded="md"
+        p={3}
+      >
+        <Grid templateColumns="repeat(3, 1fr)">
+          <Spacer></Spacer>
           <Text>TOMB</Text>
-          <ButtonGroup border="4px">
-            <Button>+</Button>
-            
+          <Button p="none" border="1px" w={12} h={6} ml={8} py={4}>
+            <Text fontSize="16px" pr={0.5}>+</Text>
             <Image
-            boxSize='20px'
-            src="https://cdn.iconscout.com/icon/free/png-256/metamask-2728406-2261817.png"/>
-          </ButtonGroup>
+              ml={0}
+              src="https://cdn.iconscout.com/icon/free/png-256/metamask-2728406-2261817.png"
+            />
+          </Button>
+        </Grid>
+        <Box />
+        <Stack justifyContent="center">
+          <Center>
+            <Image
+              rounded="full"
+              bg="gray"
+              src="https://tomb.finance/static/media/crypto_tomb_cash.f2b44ef4.svg"
+              alt="Dan Abramov"
+              boxSize="70px"
+            />
+          </Center>
+          <Text fontSize="sm">Current Price</Text>
+          <Text fontSize="md">1.0917FTM</Text>
+          <Text fontSize="xs">$3.27</Text>
+          <Text fontSize="xx-small">Market Cap: $480563748.57</Text>
+          <Text fontSize="xx-small">Circulating Supply: 146961391</Text>
+          <Text fontSize="xx-small">Total Supply: 146961523</Text>
         </Stack>
-        <Box/>
-        <Image 
-          rounded="full"
-          bg="gray"
-          src='https://tomb.finance/static/media/crypto_tomb_cash.f2b44ef4.svg' 
-          alt='Dan Abramov' 
-          boxSize='70px'
-        />
-        <Text fontSize="sm">Current Price</Text>
-        <Text fontSize="md">1.0917FTM</Text>
-        <Text fontSize="xs">$3.27</Text>
-        <Text fontSize="xx-small">Market Cap: $480563748.57</Text>
-        <Text fontSize="xx-small">Circulating Supply: 146961391</Text>
-        <Text fontSize="xx-small">Total Supply: 146961523</Text>
       </Box>
-      
-      <Box textAlign="center"  backgroundColor={'white'} color="black" opacity="90%" rounded="md" p={3}>
-        <HStack >
+
+      <Box
+        textAlign="center"
+        backgroundColor={"white"}
+        color="black"
+        opacity="90%"
+        rounded="md"
+        p={3}
+      >
+        <Grid templateColumns="repeat(3, 1fr)">
+          <Spacer></Spacer>
           <Text>TOMB</Text>
-          <ButtonGroup border="4px">
-            <Button>+</Button>
-            
+          <Button p="none" border="1px" w={12} h={6} ml={8} py={4}>
+            <Text fontSize="16px" pr={0.5}>+</Text>
             <Image
-            boxSize='20px'
-            src="https://cdn.iconscout.com/icon/free/png-256/metamask-2728406-2261817.png"/>
-          </ButtonGroup>
-        </HStack>
-        <Box/>
-        <Image 
-          rounded="full"
-          bg="gray"
-          src='https://tomb.finance/static/media/crypto_tomb_share.bf1a6c52.svg' 
-          alt='Dan Abramov' 
-          boxSize='70px'
-        />
-        <Text fontSize="sm">Current Price</Text>
-        <Text fontSize="md">1.0917FTM</Text>
-        <Text fontSize="xs">$3.27</Text>
-        <Text fontSize="xx-small">Market Cap: $480563748.57</Text>
-        <Text fontSize="xx-small">Circulating Supply: 146961391</Text>
-        <Text fontSize="xx-small">Total Supply: 146961523</Text>
-      </Box>
-      <Box textAlign="center"  backgroundColor={'white'} color="black" opacity="90%" rounded="md" p={3} >
-        <HStack >
+              ml={0}
+              src="https://cdn.iconscout.com/icon/free/png-256/metamask-2728406-2261817.png"
+            />
+          </Button>
+        </Grid>
+        <Box />
+        <Stack justifyContent="center">
+          <Center>
+            <Image
+              rounded="full"
+              bg="gray"
+              src="https://tomb.finance/static/media/crypto_tomb_share.bf1a6c52.svg"
+              alt="Dan Abramov"
+              boxSize="70px"
+            />
+          </Center>
+          <Text fontSize="sm">Current Price</Text>
+          <Text fontSize="md">1.0917FTM</Text>
+          <Text fontSize="xs">$3.27</Text>
+          <Text fontSize="xx-small">Market Cap: $480563748.57</Text>
+          <Text fontSize="xx-small">Circulating Supply: 146961391</Text>
+          <Text fontSize="xx-small">Total Supply: 146961523</Text>
+        </Stack>
+      </Box>      
+      <Box
+        textAlign="center"
+        backgroundColor={"white"}
+        color="black"
+        opacity="90%"
+        rounded="md"
+        p={3}
+      >
+        <Grid templateColumns="repeat(3, 1fr)">
+          <Spacer></Spacer>
           <Text>TOMB</Text>
-          <ButtonGroup border="4px">
-            <Button>+</Button>
-            
+          <Button p="none" border="1px" w={12} h={6} ml={8} py={4}>
+            <Text fontSize="16px" pr={0.5}>+</Text>
             <Image
-            boxSize='20px'
-            src="https://cdn.iconscout.com/icon/free/png-256/metamask-2728406-2261817.png"/>
-          </ButtonGroup>
-        </HStack>
-        <Box/>
-        <Image 
-          rounded="full"
-          bg="gray"
-          src='https://tomb.finance/static/media/crypto_tomb_bond.f3331ee8.svg' 
-          alt='Dan Abramov' 
-          boxSize='70px'
-        />
-        <Text fontSize="sm">Current Price</Text>
-        <Text fontSize="md">1.0917FTM</Text>
-        <Text fontSize="xs">$3.27</Text>
-        <Text fontSize="xx-small">Market Cap: $480563748.57</Text>
-        <Text fontSize="xx-small">Circulating Supply: 146961391</Text>
-        <Text fontSize="xx-small">Total Supply: 146961523</Text>
+              ml={0}
+              src="https://cdn.iconscout.com/icon/free/png-256/metamask-2728406-2261817.png"
+            />
+          </Button>
+        </Grid>
+        <Box />
+        <Stack justifyContent="center">
+          <Center>
+            <Image
+              rounded="full"
+              bg="gray"
+              src="https://tomb.finance/static/media/crypto_tomb_bond.f3331ee8.svg"
+              alt="Dan Abramov"
+              boxSize="70px"
+            />
+          </Center>
+          <Text fontSize="sm">Current Price</Text>
+          <Text fontSize="md">1.0917FTM</Text>
+          <Text fontSize="xs">$3.27</Text>
+          <Text fontSize="xx-small">Market Cap: $480563748.57</Text>
+          <Text fontSize="xx-small">Circulating Supply: 146961391</Text>
+          <Text fontSize="xx-small">Total Supply: 146961523</Text>
+        </Stack>
       </Box>
     </SimpleGrid>
-    <Footer/>
+    <Footer />
   </Box>
-)
+);
 
-export default Home
+export default Home;
